337d159ecdf9ca31607c71ec40d282c04bb258a2
Ferdaus Kawsar <ferdaus.kawsar@gmail.com>
2019-01-28 09:58:22 -0500

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package spring2019.se1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("adding 2 and 5 is ="+ add(2,5) );
+	}
+
+	static int add(int a, int b)
+	{
+		return a+b;
+	}
+
+	static int subtract(int a, int b)
+	{
+		return a-b;
+	}
+	
+	static int division(int a, int b)
+	{
+		return a/b;
+	}
+}
